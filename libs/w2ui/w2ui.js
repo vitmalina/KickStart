89480ef77cfc6d00323db89d3615162a144c9784
vitmalina <vitmalina@gmail.com>
2013-07-06 20:50:58 -0700

    Updated w2ui library

--- libs/w2ui/w2ui.js
+++ libs/w2ui/w2ui.js
@@ -3770,7 +3770,7 @@ w2utils.keyboard = (function (obj) {
 			var t2 = Math.floor(records[0].scrollTop / this.recordHeight + 1) + Math.floor(records.height() / this.recordHeight);
 			if (t1 > this.buffered) t1 = this.buffered;
 			if (t2 > this.buffered) t2 = this.buffered;
-			$('#grid_'+ this.name + '_footer .w2ui-footer-right').html(w2utils.format(this.offset + t1) + '-' + w2utils.format(this.offset + t2) + ' of ' +	w2utils.format(this.total) + 
+			$('#grid_'+ this.name + '_footer .w2ui-footer-right').html(w2utils.format(this.offset + t1) + '-' + w2utils.format(this.offset + t2 + 1) + ' of ' +	w2utils.format(this.total) + 
 					(this.url != '' ? ' (buffered '+ w2utils.format(this.buffered) + (this.offset > 0 ? ', skip ' + w2utils.format(this.offset) : '') + ')' : '')
 			);
 			if (!this.fixedBody || this.total <= 300) return;
