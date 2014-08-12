84f01f1da362c2066dda8845fdc2960364f56ce1
Vitali Malinouski <vitmalina@gmail.com>
2014-08-11 19:22:38 -0700

    Fised small bug, close issue #7

--- setup/db-data.sql
+++ setup/db-data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users(userid, fname, lname, email, login, pass, super, hidden, deleted, last_userid, last_update)
-VALUES (1, 'admin', 'admin', '', 'admin', md5('admin'), true, false, false, 1, now());
+VALUES (1, 'Admin', 'Admin', '', 'admin', md5('admin'), true, false, false, 1, now());
