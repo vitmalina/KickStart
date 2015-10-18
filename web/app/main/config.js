37b7874f7822aa41dd20b28076a495f19702429a
Vitali Malinouski <vitmalina@gmail.com>
2015-10-17 19:16:56 -0700

    Small css fix

--- web/app/main/config.js
+++ web/app/main/config.js
@@ -11,9 +11,9 @@ config = {
         style: 'background-color: #bbb;',
         panels: [
             { type: 'top', size: '20px', overflow: 'hidden', hidden: true },
-            { type: 'left', size: '160px', minSize: 100, resizable: true },
+            { type: 'left', size: '160px', minSize: 100, resizable: true, style: 'border-right: 1px solid silver' },
             { type: 'main', overflow: 'hidden', style: 'background-color: white;' },
-            { type: 'right', size: '400px', resizable: true, hidden: true },
+            { type: 'right', size: '400px', resizable: true, hidden: true, style: 'border-left: 1px solid silver' },
             { type: 'preview', size: '200px', overflow: 'hidden', hidden: true, resizable: true },
             { type: 'bottom', size: '40px', hidden: true }
         ]
